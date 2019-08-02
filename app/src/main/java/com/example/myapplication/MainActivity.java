df9ed426e6ac56ba8757b2d1894f831c64a5bc98
hstk1993 <541488503@qq.com>
2019-08-02 09:02:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.example.myapplication.adapter.VpAdapter;
+import com.example.myapplication.fragment.AFragment;
+import com.example.myapplication.fragment.BFragment;
+import com.example.myapplication.fragment.CFragment;
+import com.example.myapplication.fragment.DFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mVp;
+    private TabLayout mTab;
+    private ArrayList<Fragment> mFragments;
+    private VpAdapter mVpAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initFragment();
+    }
+
+    private void initFragment() {
+        mFragments = new ArrayList<>();
+        mFragments.add(new AFragment());
+        mFragments.add(new BFragment());
+        mFragments.add(new CFragment());
+        mFragments.add(new DFragment());
+        mVpAdapter = new VpAdapter(getSupportFragmentManager(), mFragments);
+        mVp.setAdapter(mVpAdapter);
+        mTab.setupWithViewPager(mVp);
+        mTab.getTabAt(0).setText("大娃").setIcon(R.mipmap.ic_launcher);
+        mTab.getTabAt(1).setText("二娃").setIcon(R.mipmap.ic_launcher);
+        mTab.getTabAt(2).setText("三娃").setIcon(R.mipmap.ic_launcher);
+        mTab.getTabAt(3).setText("四娃").setIcon(R.mipmap.ic_launcher);
+    }
+
+    private void initView() {
+        mVp = findViewById(R.id.fm);
+        mTab = findViewById(R.id.tab);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/adapter/VpAdapter.java
@@ -0,0 +1,26 @@
+package com.example.myapplication.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class VpAdapter extends FragmentStatePagerAdapter {
+    private ArrayList<Fragment> mFragments;
+
+    public VpAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+        super(fm);
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
